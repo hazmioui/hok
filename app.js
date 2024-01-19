6d75542f7ed27823e82651ed8a93a654055fd262
hazmioui <133530942+hazmioui@users.noreply.github.com>
2024-01-19 06:19:17 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 
 
     var firebaseConfig = {
-    apiKey: "API KEY",
-    authDomain: "AUTH DOMAIN",
-    databaseURL: "DATABSE URL",
-    projectId: "PROJECT ID",
-    storageBucket: "BUCKET URL",
-    messagingSenderId: "SENDER ID",
-    appId: "APP ID"
+    apiKey: "AIzaSyBwGEoUWragyCJc1hJifbZRThBP1TVJiVs",
+    authDomain: "insta-e10de.firebaseapp.com",
+    databaseURL: "https://insta-e10de-default-rtdb.firebaseio.com/",
+    projectId: "insta-e10de",
+    storageBucket: "insta-e10de.appspot.com",
+    messagingSenderId: "289652294631",
+    appId: "1:289652294631:android:720083e04fd59fa03f3342"
   }; 
  
 ////Do Not Edit Below This
